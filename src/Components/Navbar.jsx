a3a159043c3c4184cf667171d0eec219457d8ac7
Rishit Ranjan <rishitranjan131@gmaul.com>
2025-07-12 16:31:11 +0530

    raw

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <div className="fixed z-[999] w-full px-20 py-8 font-['Neue Montreal'] flex justify-between">
-        <div>
-          <h1 className="text-2xl font-bold font-['Neue Montreal']">SkillSwap</h1>
-        </div>
-        <div className="links flex gap-11">
-            {["Services", "Our Work", "About us", "Insights", "Contact us", "Requests", "User Profile"].map((item, index) => (
-                <a 
-                  key={index} 
-                  href={
-                    item === "User Profile" ? "/profile" : 
-                    item === "Requests" ? "/requests" : 
-                    "#"
-                  } 
-                  className={`text-lg capitalize font-light ${index === 6 ? "ml-32" : ""}`}
-                >
-                  {item}
-                </a>
-            ))}
-        </div>
+      <div>
+        <Link to="/" className="text-2xl font-bold font-['Neue Montreal']">SkillSwap</Link>
+      </div>
+      <div className="links flex gap-11">
+        {["Services", "Our Work", "About us", "Insights", "Contact us", "Requests", "User Profile"].map((item, index) => (
+          <Link
+            key={index}
+            to={
+              item === "User Profile" ? "/profile" : 
+              item === "Requests" ? "/requests" : 
+              "#"
+            }
+            className={`text-lg capitalize font-light ${index === 6 ? "ml-32" : ""}`}
+          >
+            {item}
+          </Link>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
